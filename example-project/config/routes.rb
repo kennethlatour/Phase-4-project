2c972e3fd8b9d1eebef7960ebc34d98d84d43711
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-25 14:11:41 -0400

    fixes

--- example-project/config/routes.rb
+++ example-project/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects only: [:create, :index, :show, :destroy, :update]
-  resources :user_projects only: [:create, :index, :show, :destroy, :update]
-  resources :users only: [:create, :index, :show, :destroy, :update]
+  resources :projects, only: [:create, :index, :show, :destroy, :update]
+  resources :user_projects, only: [:create, :index, :show, :destroy, :update]
+  resources :users, only: [:create, :index, :show, :destroy, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/hello', to: 'application#hello_world'
   # Defines the root path route ("/")
