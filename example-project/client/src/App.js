31b76df57b6569005c5a52ff70633ddc33bf2098
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-26 09:07:19 -0400

    react-router

--- example-project/client/src/App.js
+++ example-project/client/src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
+import { Switch, Route } from 'react-router-dom';
 import Home from "./Home";
 import ProjectsContainer from "./ProjectsContainer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Project from "./Project";
 
 
 function App() {
@@ -13,7 +15,17 @@ function App() {
 
   return (
   <div className="overlay">
-    <Home />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/projects">
+        <ProjectsContainer />
+      </Route>
+      <Route exact path="/projects/:id">
+        <Project/>
+      </Route>
+    </Switch>
     
     {/* <ProjectsContainer /> */}
 
