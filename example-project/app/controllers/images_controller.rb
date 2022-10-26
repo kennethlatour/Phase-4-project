89e5561e2001057c842d6b17e011a577066cee7e
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-26 14:35:05 -0400

    added images

--- example-project/app/controllers/images_controller.rb
+++ example-project/app/controllers/images_controller.rb
@@ -1,2 +1,12 @@
 class ImagesController < ApplicationController
+    def create
+        image = Image.create!(image_params)
+        render json: image, status: :created
+    end
+
+    private
+
+    def image_params
+        params.permit(:url, :description, :project_id)
+    end
 end
